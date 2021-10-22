a94efda7f7df5db770f4cc7e35eb25cdb0fa7f85
Rodrigo Ferreira <77054411+RodrigoFerreira055@users.noreply.github.com>
2021-10-21 22:02:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMedia = {
+  github: 'rodrigoferreira055',
+  youtube: '',
+  facebook: 'rodrigoice',
+  instagram: 'rodrigofn055',
+  twitter: 'roodrigonuunes'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    // alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  //promessa
+  fetch(url).then(response => {
+    response.json().then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+  })
+}
+
+getGitHubProfileInfos()
